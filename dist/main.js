9c1f85cdc6139e9a47aa4cde92e13a4d8e57c1b2
Case Wylie <cmwylie19@defenseunicorns.com>
2023-06-16 13:44:37 -0400

    [TASK] Generic Interfaces & Implementations
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,38 @@
+var ContactStatus;
+(function (ContactStatus) {
+    ContactStatus["Online"] = "Online";
+    ContactStatus["Offline"] = "Offline";
+})(ContactStatus || (ContactStatus = {}));
 class Person {
-    name;
-    constructor() {
-        this.name = "John Doe";
+    thing;
+    constructor(thing) {
+        console.log(JSON.stringify(thing, undefined, 2));
+        this.thing = thing;
+        // this.person.age = person.age;
+        // this.person.city = person.city;
     }
+    /**
+     * getName
+     */
     getName() {
-        console.log(this.name);
+        console.log(this.thing.name);
     }
 }
-(() => new Person().getName())();
+let x = 5;
+let personName = "John Doe";
+let me = {
+    name: "Casey Wylie",
+    age: 34,
+    city: "Miami",
+    neighborhood: "Brickell"
+};
+// An animal Interface that has a generic type of IPerson
+let Archie = {
+    name: "Archie",
+    age: 2,
+    type: "Dog",
+    owner: me
+};
+console.log(me.neighborhood);
+(() => new Person(me).getName())();
+(() => new Person(Archie).getName())();
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,68 @@
-class Person {
-    name: string;
-
-    constructor() {
-        this.name= "John Doe"
-    }
-
-    getName() {
-         console.log(this.name)
-    }
+// interface that has all fields from INeighbor
+interface IPerson extends INeighbor{
+    name?: string;
+    age?: number;
+    city?: string;
 }
 
-(()=>new Person().getName())();
+// interface with a generic
+interface IAnimal<T>{
+    name?: string;
+    age?: number;
+    type?: string;
+    //GenericType
+    owner?: T;
+}
+
+interface INeighbor {
+    Building?: string;
+    neighborhood?: Barrio;
+}
+enum ContactStatus {
+    Online = "Online",
+    Offline = "Offline",
+}
+type Barrio = "Little Havana" | "Wynwood" | "Brickell" | "Downtown" | "Coral Gables";
+
+// Person class with a generic type of IPerson or IAnimal that has a generic type of IPerson
+class Person<T extends IPerson, T1 extends IAnimal<IPerson>> {
+    thing: T;
+
+    constructor(thing: T) {
+        console.log(JSON.stringify(thing,undefined,2))
+        this.thing= thing;
+        // this.person.age = person.age;
+        // this.person.city = person.city;
+    }
+    /**
+     * getName
+     */
+    getName() {
+         console.log(this.thing.name)
+    }
+
+}
+
+
+let x: number = 5;
+let personName: string = "John Doe";
+
+let me: IPerson = {
+    name: "Casey Wylie",
+    age: 34,
+    city: "Miami",
+    neighborhood: "Brickell"
+};
+
+// An animal Interface that has a generic type of IPerson
+let Archie: IAnimal<IPerson> = {
+    name: "Archie",
+    age: 2,
+    type: "Dog",
+    owner: me
+};
+console.log(me.neighborhood);
+
+(()=>new Person(me).getName())();
+
+(()=>new Person(Archie).getName())();
